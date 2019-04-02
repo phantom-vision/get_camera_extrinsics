72697193e2456ba0ed961df750858eb6aba51cfd
harsha5 <harshbitsp@gmail.com>
2019-04-02 18:31:08 -0400

    Extrinsic pipeline completed

--- src/get_camera_extrinsics.py
+++ src/get_camera_extrinsics.py
@@ -4,6 +4,7 @@ import rospy
 import numpy as np
 import cv2
 import pdb
+import os, sys
 
 def get_image_points(K,pts_in_alvar_frame):
     epsilon = 0.0000001
@@ -16,15 +17,26 @@ def get_image_points(K,pts_in_alvar_frame):
     return image_points[:-1,:]
 
 def get_extrinsics(K,pts_in_alvar_frame,pts_in_world_frame):
-    rospy.loginfo("In get_extrinsics")
+    #rospy.loginfo("In get_extrinsics")
     image_points = get_image_points(K,pts_in_alvar_frame)
+    image_points = image_points.astype(float)
+    pts_in_world_frame = pts_in_world_frame.astype(float)
+    dist_coef = np.zeros(4)
+    _, rvecs, tvecs, inliers = cv2.solvePnPRansac(pts_in_world_frame.T, image_points.T, K, dist_coef,iterationsCount=10000)
+    # retval, rvec, tvec= cv2.solvePnP(pts_in_world_frame.T, image_points.T, K, dist_coef)
+    rot_mat,jacob = cv2.Rodrigues(rvecs)
+    H = np.vstack((np.hstack((rot_mat,tvecs)),np.array([0,0,0,1])))
+    return H 
 
+#Ideas to fix: Use np float, use dist_coef as None ,Use ransac
 
     return H
 
 if __name__ == '__main__':
     rospy.init_node('get_camera_extrinsics', anonymous = True)
+    #Use absolute paths if the loading the below files give an error
     K = np.load("real_sense_intrinsics.npy")
     pts_in_alvar_frame = np.load("pts_in_alvar_frame.npy")
     pts_in_world_frame = np.load("pts_in_world_frame.npy")
     H = get_extrinsics(K,pts_in_alvar_frame,pts_in_world_frame)
+    rospy.loginfo("Extrinsics obtained")
