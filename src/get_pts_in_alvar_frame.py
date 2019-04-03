329d564a5bb1d7f74c1d7dc4a2cd50255fcb6249
harsha5 <harshbitsp@gmail.com>
2019-04-03 14:51:01 -0400

    get_pts_in_alvar_frame completed

--- src/get_pts_in_alvar_frame.py
+++ src/get_pts_in_alvar_frame.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-from geometry_msgs.msg import position,pose
-import ar_track_alvar_msgs
+from geometry_msgs.msg import PoseStamped
+from ar_track_alvar_msgs.msg import AlvarMarkers
 
-pts_in_alvar_frame_X = 0.0
-pts_in_alvar_frame_Y = 0.0
-pts_in_alvar_frame_Z = 0.0
-
-def callback(msg):
-    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    global pts_in_alvar_frame_X = msg.pose.pose.position.x
-    global pts_in_alvar_frame_Y = msg.pose.pose.position.y
-    global pts_in_alvar_frame_Z = msg.pose.pose.position.z
-    rospy.loginfo("I heard ", pts_in_alvar_frame_X)
     
-def listener():
+def alvar_pose_listener():
     rospy.init_node('get_pts_in_alvar_frame', anonymous=True)
 
-    rospy.Subscriber("ar_pose_marker", AlvarMarker, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    msg = rospy.wait_for_message("ar_pose_marker", AlvarMarkers)
+    try:
+        pts_in_alvar_frame_X = msg.markers[0].pose.pose.position.x
+        pts_in_alvar_frame_Y = msg.markers[0].pose.pose.position.y
+        pts_in_alvar_frame_Z = msg.markers[0].pose.pose.position.z
+        rospy.loginfo("AR tag in alvar captured \n")
+        # print(pts_in_alvar_frame_X))
+        # print(pts_in_alvar_frame_Y)
+        # print(pts_in_alvar_frame_Z)
+        return [pts_in_alvar_frame_X,pts_in_alvar_frame_Y,pts_in_alvar_frame_Z]
+    except IndexError:
+        rospy.loginfo(" AR tag not captured \n")
+        alvar_pose_listener()
 
 if __name__ == '__main__':
-    listener()
+    pts_in_alvar_frame_list = alvar_pose_listener()
